5a7c63158a4dbe865e1b894f7252fd4690d34154
Anmol Kesar <Anmol.Kesar@tadigital.com>
2020-08-16 22:32:44 +0530

    overwrite URL in main

--- src/main.js
+++ src/main.js
@@ -5,8 +5,8 @@ import axios from 'axios'
 import 'bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
-axios.defaults.baseURL = 'https://localhost:5001/api/'
-//axios.defaults.baseURL = 'https://anonymousfeedback20200804045526.azurewebsites.net/api/'
+//axios.defaults.baseURL = 'https://localhost:5001/api/'
+axios.defaults.baseURL = 'https://anonymousfeedback20200804045526.azurewebsites.net/api/'
 Vue.config.productionTip = false
 
 new Vue({
